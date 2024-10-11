35afc2b59585760c1cc48ef36c15f6e734abb56f
Charbel Rouhana <charbelmrouhana@gmail.com>
2024-10-11 22:21:42 +0200

    home page with navbar created - dark theme applied

---
+++ components/providers/RootProviders.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { ThemeProvider } from "next-themes";
+import React from "react";
+
+export default function RootProviders({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
