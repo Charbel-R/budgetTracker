ff1897f5dee9284b1edc6258c43962f066ba9408
Charbel Rouhana <charbelmrouhana@gmail.com>
2024-10-14 15:05:30 +0200

    Dashboard History Section Added

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -5,6 +5,7 @@ import { redirect } from "next/navigation";
 import React from "react";
 import { CreateTransactionDialog } from "./_components/CreateTransactionDialog";
 import Overview from "./_components/Overview";
+import History from "./_components/History";
 
 export default async function Dashboard() {
   const user = await currentUser();
@@ -50,6 +51,7 @@ export default async function Dashboard() {
         </div>
       </div>
       <Overview userSettings={userSettings} />
+      <History userSettings={userSettings} />
     </div>
   );
 }
